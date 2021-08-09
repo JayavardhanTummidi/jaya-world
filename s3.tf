0edca7efcd29da64a4e2cfad5b44866964647007
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-08-09 19:44:48 -0400

    s3

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket-jaya-world"
+  bucket = "my-s3-bucket-apple-world"
   acl    = "private"
 
   versioning = {
