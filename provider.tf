be8bb3477320af5ec73948215e9ac71b0ac643f7
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-08-09 19:27:06 -0400

    begining-try now

--- provider.tf
+++ provider.tf
@@ -1,7 +1,13 @@
 provider "aws" {
     region = "us-east-1"
-    variable "aws_access_key_id" {}
-    variable "aws_secret_access_key" {}
+    variable "aws_access_key_id" {
+        type = string
+        default = "AKIAT6BQNWYUXJOD7HMB"
+    }
+    variable "aws_secret_access_key" {
+        type = string
+        default = "FTR88ikuc7125mfcGau6kP6BqiMPV+w4Xcsd7m8R"
+    }
     access_key = var.aws_access_key_id
     secret_key = var.aws_secret_access_key
 }
