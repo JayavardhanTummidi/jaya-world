28e784cfa82993db1846656a1ca6d91b0949fb26
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-08-11 20:21:50 -0400

    s3

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 module "jayaworld-s3-bucket" {
   source  = "app.terraform.io/jaya-world/jayaworld-s3-bucket/aws"
-  version = "0.7.0"
+  version = "0.8.0"
   # insert required variables here
   bucket_name = "jaya-first-module-hello"
 }
