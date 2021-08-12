c2868563885f8352a1d5f2c23e9e0f25a0aa018f
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-08-11 21:12:26 -0400

    s3

--- variables.tf
+++ variables.tf
@@ -1,2 +1,8 @@
-variable "aws_access_key_id" {}
-variable "aws_secret_access_key" {}
+variable "aws_access_key_id" {
+  type    = string
+  default = "AKIAVGLA7FRWMSC4D7GD"
+}
+variable "aws_secret_access_key" {
+  type    = string
+  default = "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"
+} 
