1d165c8abf7f417968a90e71e56ab5710f6a8187
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-08-12 23:20:00 -0400

    s3

---
+++ s3 copy.tf
@@ -0,0 +1,11 @@
+module "jayaworld-s3-bucket" {
+  source  = "app.terraform.io/jaya-world/jayaworld-s3-bucket/aws"
+  version = "0.23.0"
+  # insert required variables here
+  bucket_name = "jaya-first-module-hello-2"
+  acl = "private"
+  tags = {
+    bucket = "s3 bucket"
+    owner = "jaya-world"
+  }
+}
