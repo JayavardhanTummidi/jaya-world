9a8d67d108d3555983585d44cb28200e6ecec37c
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-08-14 11:05:55 -0400

    s3

--- s3.tf
+++ s3.tf
@@ -4,6 +4,7 @@ module "jayaworld-s3-bucket" {
   # insert required variables here
   bucket_name = "jaya-first-module-hello-2"
   acl = "private"
+  aws_kms_key_arn = "arn:aws:kms:us-east-1:357222526060:key/07a5482e-0c43-4f2e-9e30-a0527bf7ab27"
   tags = {
     bucket = "s3 bucket"
     owner = "jaya-world"
