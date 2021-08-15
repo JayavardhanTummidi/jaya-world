c6eed9c90c5733cba9e6d3c1e0bfce078a1f4a3c
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-08-15 11:35:06 -0400

    s3

--- s3.tf
+++ s3.tf
@@ -1,10 +1,9 @@
 module "jayaworld-s3-bucket" {
   source  = "app.terraform.io/jaya-world/jayaworld-s3-bucket/aws"
-  version = "0.37.0"
+  version = "0.42.0"
   # insert required variables here
-  bucket_name = "jaya-first-module-hello-6"
+  bucket_name = "jaya-first-module-hello-5"
   acl = "private"
-  #policy = "./s3-policy.json"
   aws_kms_key_arn = module.jayaworld-kms-key.arn
   tags = {
     bucket = "s3 bucket"
